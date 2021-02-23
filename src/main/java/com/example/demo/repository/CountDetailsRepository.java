179e6233c3ddb735dd90ec7b01134fbb93cc32a9
marszm <mariusz.szmer@gmail.com>
2021-02-23 21:31:48 +0100

    missing adnotation

--- src/main/java/com/example/demo/repository/CountDetailsRepository.java
+++ src/main/java/com/example/demo/repository/CountDetailsRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.CountDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountDetailsRepository extends JpaRepository<CountDetails, Integer> {
 
     CountDetails findByLogin(String login);
