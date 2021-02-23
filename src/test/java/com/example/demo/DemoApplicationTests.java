6dc5535e9742250fc7433f3a8749df885683d10f
marszm <mariusz.szmer@gmail.com>
2021-02-23 22:18:26 +0100

    added tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.controller.UserController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
 class ProductApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    void hello() throws Exception {
+        RequestBuilder request = get("/users/login");
+        MvcResult result = mvc.perform(request).andReturn();
+        assertEquals("marszm", result.getResponse().getContentAsString());
+    }
 }
